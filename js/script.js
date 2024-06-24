bc24934c2da55a95c4bd06f2ec68087d8bd0c57d
João Luciano <lucianoangeys@gmail.com>
2024-06-24 10:13:11 -0300

    Mais álbuns

--- js/script.js
+++ js/script.js
@@ -86,6 +86,95 @@ const volume4 = {
   color: "#526193",
 };
 
+const volume5 = {
+  cover: "img/volume-5.jpg",
+  title: "Leandro & Leonardo Vol. 5",
+  year: 1991,
+  numberOfTracks: 12,
+  tracks: [
+    "Sonho por Sonho",
+    "Gostoso Sentimento",
+    "Não Aprendi a Dizer Adeus",
+    "Bate Coração",
+    "Sabor de Mim",
+    "Me Dê Um Sinal",
+    "Mensagem",
+    "Estou na Mão Dessa Mulher",
+    "A Rotina (Fim de Semana)",
+    "Não Olhe Assim",
+    "Amores São Coisas da Vida",
+    "Paz na Cama",
+  ],
+  color: "#5e4d82",
+};
+
+const volume6 = {
+  cover: "img/volume-6.jpg",
+  title: "Leandro & Leonardo Vol. 6",
+  year: 1992,
+  numberOfTracks: 12,
+  tracks: [
+    "O Que Eu Sinto É Amor",
+    "Um Arraso de Mulher",
+    "Fim de Semana Sem Você",
+    "Conta Pra Ela",
+    "Mais Uma Noite Sem Você",
+    "Você É Doida Demais",
+    "Não Durmo Sem Ela",
+    "Amor Vampiro",
+    "Temporal de Amor",
+    "Esta Noite Foi Maravilhosa (Wonderful Tonight)",
+    "Vem Fazer Amor Comigo",
+    "Chega",
+  ],
+  color: "#a68675",
+};
+
+const volume7 = {
+  cover: "img/volume-7.jpg",
+  title: "Leandro & Leonardo Vol. 7",
+  year: 1993,
+  numberOfTracks: 12,
+  tracks: [
+    "Bobo",
+    "Mexe Mexe",
+    "Quem é",
+    "Outra Chance",
+    "Seja Bem-Vinda",
+    "Em Nome do Amor",
+    "Quero Ser o Seu Dono",
+    "Gosto Dela",
+    "A Vida Tem Dessas Coisas",
+    "Uma História Sem Fim",
+    "O Quanto Nosso Amor Valeu",
+    "Diz Que Me Ama",
+  ],
+  color: "#ea603d",
+};
+
+const volume8 = {
+  cover: "img/volume-8.jpg",
+  title: "Leandro & Leonardo Vol. 8",
+  year: 1994,
+  numberOfTracks: 13,
+  tracks: [
+    "Mulher Brasileira",
+    "Não Sei Mais Dormir Sozinho",
+    "25 Horas por Dia",
+    "Saudades Dela",
+    "Dor de Amor Não Tem Jeito",
+    "Índia",
+    "Diga Pra Mim (Philosopher)",
+    "Casa Separa",
+    "Cavaleiro do Asfalto",
+    "Brigar Com Ele",
+    "Dodói de Amor",
+    "Separados",
+    "Achei Uma Louca",
+  ],
+  color: "#e0d8dc",
+};
+
 const body = document.body;
 const main = document.querySelector("main");
 const aside = document.querySelector("aside");
@@ -95,94 +184,55 @@ const albumTitle = document.getElementById("albumTitle");
 const albumYear = document.getElementById("year");
 const playlist = document.getElementById("playlist");
 
-document.getElementById("alb1").addEventListener("click", () => {
-  albumCover.src = explosaoDeDesejos.cover;
-  albumTitle.innerText = explosaoDeDesejos.title;
-  albumYear.innerText = explosaoDeDesejos.year;
+function displayAlbum(album, colorText) {
+  albumCover.src = album.cover;
+  albumTitle.innerText = album.title;
+  albumYear.innerText = album.year;
 
-  body.style.backgroundImage = `url("${explosaoDeDesejos.cover}")`;
-  body.style.color = "black";
-  main.style.backgroundColor = explosaoDeDesejos.color;
-  main.style.color = "black";
-  aside.style.backgroundColor = explosaoDeDesejos.color;
-  aside.style.color = "black";
-  footer.style.backgroundColor = explosaoDeDesejos.color;
+  body.style.backgroundImage = `url("${album.cover}")`;
+  body.style.color = colorText;
+  main.style.backgroundColor = album.color;
+  main.style.color = colorText;
+  aside.style.backgroundColor = album.color;
+  aside.style.color = colorText;
+  footer.style.backgroundColor = album.color;
+  playlist.innerHTML = ""; // Limpa a lista de músicas
 
-  for (let i = 0; i < explosaoDeDesejos.numberOfTracks; i++) {
+  for (let i = 0; i < album.numberOfTracks; i++) {
     const li = document.createElement("li");
-    playlist.append(li);
-
-    playlist.childNodes.forEach((f, v) => {
-      f.innerText = explosaoDeDesejos.tracks[v];
-    });
+    li.innerText = album.tracks[i];
+    playlist.appendChild(li);
   }
-});
+}
 
-document.getElementById("alb2").addEventListener("click", () => {
-  albumCover.src = volume2.cover;
-  albumTitle.innerText = volume2.title;
-  albumYear.innerText = volume2.year;
+document
+  .getElementById("alb1")
+  .addEventListener("click", () => displayAlbum(explosaoDeDesejos, "black"));
 
-  body.style.backgroundImage = `url("${volume2.cover}")`;
-  body.style.color = "white";
-  main.style.backgroundColor = volume2.color;
-  main.style.color = "white";
-  aside.style.backgroundColor = volume2.color;
-  aside.style.color = "white";
-  footer.style.backgroundColor = volume2.color;
+document
+  .getElementById("alb2")
+  .addEventListener("click", () => displayAlbum(volume2, "white"));
 
-  for (let i = 0; i < volume2.numberOfTracks; i++) {
-    const li = document.createElement("li");
-    playlist.append(li);
+document
+  .getElementById("alb3")
+  .addEventListener("click", () => displayAlbum(volume3, "black"));
 
-    playlist.childNodes.forEach((f, v) => {
-      f.innerText = volume2.tracks[v];
-    });
-  }
-});
+document
+  .getElementById("alb4")
+  .addEventListener("click", () => displayAlbum(volume4, "white"));
 
-document.getElementById("alb3").addEventListener("click", () => {
-  albumCover.src = volume3.cover;
-  albumTitle.innerText = volume3.title;
-  albumYear.innerText = volume3.year;
+document
+  .getElementById("alb5")
+  .addEventListener("click", () => displayAlbum(volume5, "white"));
 
-  body.style.backgroundImage = `url("${volume3.cover}")`;
-  body.style.color = "black";
-  main.style.backgroundColor = volume3.color;
-  main.style.color = "black";
-  aside.style.backgroundColor = volume3.color;
-  aside.style.color = "black";
-  footer.style.backgroundColor = volume3.color;
+document
+  .getElementById("alb6")
+  .addEventListener("click", () => displayAlbum(volume6, "black"));
 
-  for (let i = 0; i < volume3.numberOfTracks; i++) {
-    const li = document.createElement("li");
-    playlist.append(li);
+document
+  .getElementById("alb7")
+  .addEventListener("click", () => displayAlbum(volume7, "white"));
 
-    playlist.childNodes.forEach((f, v) => {
-      f.innerText = volume3.tracks[v];
-    });
-  }
-});
-
-document.getElementById("alb4").addEventListener("click", () => {
-  albumCover.src = volume4.cover;
-  albumTitle.innerText = volume4.title;
-  albumYear.innerText = volume4.year;
-
-  body.style.backgroundImage = `url("${volume4.cover}")`;
-  body.style.color = "white";
-  main.style.backgroundColor = volume4.color;
-  main.style.color = "white";
-  aside.style.backgroundColor = volume4.color;
-  aside.style.color = "white";
-  footer.style.backgroundColor = volume4.color;
-
-  for (let i = 0; i < volume4.numberOfTracks; i++) {
-    const li = document.createElement("li");
-    playlist.append(li);
-
-    playlist.childNodes.forEach((f, v) => {
-      f.innerText = volume4.tracks[v];
-    });
-  }
-});
+document
+  .getElementById("alb8")
+  .addEventListener("click", () => displayAlbum(volume8, "black"));
